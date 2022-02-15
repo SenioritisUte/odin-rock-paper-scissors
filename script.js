6e67712cd8d0e60366904f4e162cb168688587d9
Danny Inkley <danny@ratenow.net>
2022-02-15 14:04:36 -0700

    .

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let firstChoice = prompt('Rock, paper, or scissors?');
-let firstChoiceFixed = firstChoice[0].toUpperCase() + firstChoice.substring(1);
-
 
 let randomNumber = (Math.floor(Math.random()*3) + 1);
 
@@ -11,37 +8,48 @@ function computerPlay() {
     if (randomNumber === 3) return 'Scissors';
 }
 
+function playerPlay () {
+    let choice = prompt('Rock, paper, or scissors?');
+    let choiceFixed = choice[0].toUpperCase() + choice.substring(1);
+    return choiceFixed;
+}
+
+
 console.log(computerPlay());
 
 function playRound(playerSelection, computerSelection) {
 
-    
-    if (playerSelection === 'Rock' && computerSelection === 'Rock') return 'It is a tie. Try again!';
-    if (playerSelection === 'Rock' && computerSelection === 'Paper') return 'You lose! Paper beats rock.';
-    if (playerSelection === 'Rock' && computerSelection === 'Scissors') return 'You win! Rock beats scissors!';
-    if (playerSelection === 'Scissors' && computerSelection === 'Scissors') return 'It is a tie. Try again!';
-    if (playerSelection === 'Scissors' && computerSelection === 'Rock') return 'You lose! Rock beats Scissors.';
-    if (playerSelection === 'Scissors' && computerSelection === 'Paper') return 'You win! Scissors beats paper!';
-    if (playerSelection === 'Paper' && computerSelection === 'Paper') return 'It is a tie. Try again!';
-    if (playerSelection === 'Paper' && computerSelection === 'Scissors') return 'You lose! Scissors beats paper.';
-    if (playerSelection === 'Paper' && computerSelection === 'Rock') return 'You win! Paper beats rock!';
+    if (playerSelection === 'Rock' && computerSelection === 'Rock') return 'It is a tie.';
+    if (playerSelection === 'Rock' && computerSelection === 'Paper') return 'Player loses.';
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors') return 'Player wins.';
+    if (playerSelection === 'Scissors' && computerSelection === 'Scissors') return 'It is a tie.';
+    if (playerSelection === 'Scissors' && computerSelection === 'Rock') return 'Player loses.';
+    if (playerSelection === 'Scissors' && computerSelection === 'Paper') return 'Player wins.';
+    if (playerSelection === 'Paper' && computerSelection === 'Paper') return 'It is a tie.';
+    if (playerSelection === 'Paper' && computerSelection === 'Scissors') return 'Player loses.';
+    if (playerSelection === 'Paper' && computerSelection === 'Rock') return 'Player wins.';
 
 }
 
 
-let playerSelection = firstChoiceFixed;
+let playerSelection = playerPlay();
 let computerSelection = computerPlay();
 
 console.log(playRound(playerSelection, computerSelection));
 
 
 
-
 function game() {
+
     for (let i = 0; i < 5; i++) {
-    
+   
     playRound();
 
     }
+
+    let playerScore;
+    let computerScore;
+
 }
 
+game();
